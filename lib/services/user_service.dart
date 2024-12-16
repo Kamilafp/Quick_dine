be6ef545920f7ed3c35a3ad374d6d601998f03e5
regtat <regitaghani@gmail.com>
2024-12-17 00:06:35 +0700

    update

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -34,19 +34,20 @@ Future<ApiResponse> login(String email, String password) async {
   return apiResponse;
 }
 
-Future<ApiResponse> register(String name, String email, String password, String notelp, String role) async {
+Future<ApiResponse> register(String name, String email, String password,
+    String notelp, String role) async {
   ApiResponse apiResponse = ApiResponse();
   try {
-    final response = await http.post(Uri.parse(registerURL),
-        headers: {'Accept': 'application/json'},
-        body: {
-          'name': name,
-          'email': email,
-          'password': password,
-          'password_confirmation': password,
-          'notelp': notelp,
-          'role': role
-        });
+    final response = await http.post(Uri.parse(registerURL), headers: {
+      'Accept': 'application/json'
+    }, body: {
+      'name': name,
+      'email': email,
+      'password': password,
+      'password_confirmation': password,
+      'notelp': notelp,
+      'role': role
+    });
     switch (response.statusCode) {
       case 200:
         apiResponse.data = User.fromJson(jsonDecode(response.body));
@@ -71,9 +72,11 @@ Future<ApiResponse> register(String name, String email, String password, String 
 Future<ApiResponse> getUserDetail() async {
   ApiResponse apiResponse = ApiResponse();
   try {
-    String token=await getToken();
-    final response = await http.get(Uri.parse(userURL),
-        headers: {'Accept': 'application/json', 'Authorization': 'Bearer $token'});
+    String token = await getToken();
+    final response = await http.get(Uri.parse(userURL), headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token'
+    });
     switch (response.statusCode) {
       case 200:
         apiResponse.data = User.fromJson(jsonDecode(response.body));
@@ -96,25 +99,25 @@ Future<ApiResponse> getUserDetail() async {
 }
 
 //retreive user token from shared pref
-Future <String> getToken() async{
-  SharedPreferences pref=await SharedPreferences.getInstance();
-  return pref.getString('token')??'';
+Future<String> getToken() async {
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  return pref.getString('token') ?? '';
 }
 
-Future <int> getUserId() async{
-  SharedPreferences pref=await SharedPreferences.getInstance();
+Future<int> getUserId() async {
+  SharedPreferences pref = await SharedPreferences.getInstance();
   return pref.getInt('idUser') ?? 0;
 }
 
 //hapus token dari shared pref
-Future <bool> logout() async{
-  SharedPreferences pref=await SharedPreferences.getInstance();
+Future<bool> logout() async {
+  SharedPreferences pref = await SharedPreferences.getInstance();
   return await pref.remove('token');
 }
 
 //get base64 encoded image
-String? getStringImage(File? file){
-  if(file==null)return null;
+String? getStringImage(File? file) {
+  if (file == null) return null;
   return base64Encode(file.readAsBytesSync());
 }
 
@@ -122,15 +125,16 @@ Future<ApiResponse> getAllUsers() async {
   ApiResponse apiResponse = ApiResponse();
   try {
     String token = await getToken();
-    final response = await http.get(Uri.parse('$baseURL/allUsers'),
-    headers: {
+    final response = await http.get(Uri.parse('$baseURL/allUsers'), headers: {
       'Accept': 'application/json',
       'Authorization': 'Bearer $token'
     });
 
-    switch(response.statusCode){
+    switch (response.statusCode) {
       case 200:
-        apiResponse.data = jsonDecode(response.body)['user'].map((p) => User.fromJson(p)).toList();
+        apiResponse.data = jsonDecode(response.body)['user']
+            .map((p) => User.fromJson(p))
+            .toList();
         // we get list of posts, so we need to map each item to post model
         apiResponse.data as List<dynamic>;
         break;
@@ -141,23 +145,35 @@ Future<ApiResponse> getAllUsers() async {
         apiResponse.error = somethingWentWrong;
         break;
     }
-  }
-  catch (e){
+  } catch (e) {
     apiResponse.error = serverError;
   }
   return apiResponse;
 }
 
-Future<int> getTotalUser() async{
+Future<int> getTotalUser() async {
   String token = await getToken();
-    final response = await http.get(Uri.parse('$userURL/count'),
-    headers: {
-      'Accept': 'application/json',
-      'Authorization': 'Bearer $token'
-    });
-    if(response.statusCode==200){
-      return json.decode(response.body)['total_user'];
-    }else{
-      throw Exception('Failed to load user count');
-    }
+  final response = await http.get(Uri.parse('$userURL/count'), headers: {
+    'Accept': 'application/json',
+    'Authorization': 'Bearer $token'
+  });
+  if (response.statusCode == 200) {
+    return json.decode(response.body)['total_user'];
+  } else {
+    throw Exception('Failed to load user count');
+  }
+}
+
+Future<List<Map<String, dynamic>>> fetchUser() async {
+  String token = await getToken();
+  final response = await http.get(Uri.parse(userURL), headers: {
+    'Accept': 'application/json',
+    'Authorization': 'Bearer $token'
+  });
+  if (response.statusCode == 200) {
+    final data = json.decode(response.body)['users'] as List;
+    return data.map((item) => Map<String, dynamic>.from(item)).toList();
+  } else {
+    throw Exception('Failed to load users');
+  }
 }
