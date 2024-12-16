770648a102a26146272cfc5400c076fb64769c8d
regtat <regitaghani@gmail.com>
2024-12-16 16:04:17 +0700

    update dikit

--- lib/models/core/kantin.dart
+++ lib/models/core/kantin.dart
@@ -11,22 +11,13 @@ class Kantin {
     required this.namaKaryawan
   });
 
-  factory Kantin.fromJson(Map<String, dynamic> json) {
+  factory Kantin.fromJson(Map<String, dynamic> json){
     return Kantin(
       id: json['id'],
       namaKantin: json['nama_kantin'],
       idKaryawan: json['id_karyawan'],
       namaKaryawan: json['user']['name']
     );
-  }
-  
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'nama_kantin': namaKantin,
-      'id_karyawan': idKaryawan,
-      'nama_karyawan': namaKaryawan,
-    };
   }
 }
 
